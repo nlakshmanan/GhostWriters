351ec0f771274f9274019af2288e97ff4f73640d
Nachammai Lakshmanan <nachammailakshmanan2019@u.northwestern.edu>
2020-01-12 00:19:30 -0600

    Update train_joy.py

--- model/train_joy.py
+++ model/train_joy.py
@@ -1,3 +1,19 @@
-training_data/README.txt
+import gpt_2_simple as gpt2
+from datetime import datetime
 
-Place the training data here.
+gpt2.download_gpt2(model_name="355M")
+file_name = "./../data/joy.txt"
+
+#fine_tune
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              dataset=file_name,
+              model_name='355M',
+              steps=1000,
+              restore_from='fresh',
+              run_name='joy1',
+              print_every=10,
+              sample_every=200,
+              save_every=500
+              )
+gpt2.generate(sess)
